fd1f962939c17389d968e545fe98cbe59d429aed
Kaviyarasan <kavigunasekar185@gmail.com>
2025-05-06 22:12:13 +0530

    forms update

--- app/plantationform/bankDetails.tsx
+++ app/plantationform/bankDetails.tsx
@@ -136,7 +136,7 @@ export default function BankDetails() {
         onPress={() => router.back()}
       />
 
-      <Text style={styles.title}>Land Form</Text>
+      <Text style={styles.title}>Plantation Form</Text>
       <Text style={styles.subtitle}>Bank Details</Text>
 
       <Text style={styles.question}>44. Name of Account Holder:</Text>
