9b849e002f215bf79a3aad8b1162553e62cff7f8
Kaviyarasan <kavigunasekar185@gmail.com>
2025-08-06 21:12:00 +0530

    close appback handler in dash board

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -127,7 +127,7 @@ const DashboardScreen: React.FC = () => {
 useFocusEffect(
   React.useCallback(() => {
     const onBackPress = () => {
-      router.push('/');
+      BackHandler.exitApp();
       return true; 
     };
 
